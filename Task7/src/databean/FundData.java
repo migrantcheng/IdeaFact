dd97cef1c6c4ab3fcaded0f746890b88901c3049
migrantcheng <migrantcheng@gmail.com>
2014-01-29 23:56:52 -0500

    add FundData

---
+++ Task7/src/databean/FundData.java
@@ -0,0 +1,35 @@
+package databean;
+
+public class FundData {
+
+	private int fund_id;
+	private String name;
+	private String symbol;
+	private String price;
+	public int getFund_id() {
+		return fund_id;
+	}
+	public void setFund_id(int fund_id) {
+		this.fund_id = fund_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+	
+}
